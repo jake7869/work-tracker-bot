261b38d8db215066262896da19175cfb35d7f7ee
jake7869 <jakealfiebradley@gmail.com>
2025-04-23 11:28:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -114,12 +114,9 @@ async def update_leaderboard():
         embed.add_field(
             name=f"<@{user_id}>",
             value=(
-                f"🚗 Car: {data['car']} | 🏍️ Bike: {data['bike']}
-"
-                f"🛠️ Engine: {data['engine']} | 🚘 Car Full: {data['car_full']} | 🏍️ Bike Full: {data['bike_full']}
-"
-                f"💷 Earnings: £{data['earnings']:,}
-"
+                f"🚗 Car: {data['car']} | 🏍️ Bike: {data['bike']}\n"
+                f"🛠️ Engine: {data['engine']} | 🚘 Car Full: {data['car_full']} | 🏍️ Bike Full: {data['bike_full']}\n"
+                f"💷 Earnings: £{data['earnings']:,}\n"
                 f"⏱️ Time Clocked: {time_str}"
             ),
             inline=False
